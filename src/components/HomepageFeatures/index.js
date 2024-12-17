0fe6e40bd912b40f80255d68c8ae9fe7498cb1cf
Sophia Michel <sophiamichel89@gmx.net>
2024-12-17 09:55:58 +0100

    New Startersite text + how to upload image update

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,54 +3,46 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
-  {
-    title: 'Regel 1',
-    description: (
-      <>
-        Wenn man Daten auf einer Seite verändert soll es abgesprochen sein mit
-        dem jeweiligen Ausbilder bzw. Auszubildenden. 
-      </>
-    ),
-  },
-  {
-    title: 'Regel 2',
-    description: (
-      <>
-        Daten die es auf der Seite gibt sollen verantwortungsvoll genutzt werden.
-      </>
-    ),
-  },
-  {
-    title: 'Regel 3',
-    description: (
-      <>
-        Wenn man eine veränderung commitet soll eine sinvolle message genutzt werden.
-      </>
-    ),
-  },
+    {
+        title: 'Wichtige Information',
+        description: (
+            <>
+                <b>Bitte halten Sie sich an die Vorlagen und Hinweise. Wenn etwas unklar sein sollte, bitte nachfragen!</b>
+            </>
+        ),
+    },
+    {
+        title: 'Letzte Updates',
+        description: (
+            <>
+                <p>- How to hochladen eines Bildes geupdated</p>
+                <p>- static/img/ Ordnerstruktur erstellt</p>
+            </>
+        ),
+    },
 ];
 
 function Feature({title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={clsx('col col--6')}>
+            <div className="text--center padding-horiz--md">
+                <Heading as="h3">{title}</Heading>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
